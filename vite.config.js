9e71bf7191d80a08914aca16e0568963fd7c5fb3
Nikhil <nikhilsaraswat57@gmail.com>
2025-01-27 08:15:44 +0530

    updatedd

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Edemy",
+  base:"/Edemy/",
   plugins: [react(),
     tailwindcss(),
   ],
